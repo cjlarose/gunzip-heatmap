250aadcc3f376884508651f5696e300fc48c3898
Chris LaRose <cjlarose@gmail.com>
2016-12-04 07:40:13 -0800

    Transpile with buble

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 // rollup.config.js
 import svelte from 'rollup-plugin-svelte';
+import buble from 'rollup-plugin-buble';
 
 export default {
   entry: 'src/main.js',
@@ -9,5 +10,6 @@ export default {
     svelte({
       include: 'src/components/**.html',
     }),
+    buble(),
   ],
 };
