cbe7535fe99c18d8ed7ba5700d15133fc725fde6
Chris LaRose <cjlarose@gmail.com>
2016-12-04 10:25:58 -0800

    Use Uint8Array.from in plugin

--- rollup.config.js
+++ rollup.config.js
@@ -7,17 +7,6 @@ function binaryToBlob(options = {}) {
   const filter = createFilter(options.include, options.exclude);
   return {
     name: 'binary-to-blob',
-    intro() {
-      return `function octetsToBuffer(octetsArray) {
-  var length = octetsArray.length;
-  var buffer = new ArrayBuffer(length);
-  var view = new DataView(buffer);
-  for (var i = 0; i < length; i++) {
-    view.setUint8(i, octetsArray[i]);
-  }
-  return buffer;
-}`;
-    },
     load(id) {
       if (!filter(id)) { return null; }
       return new Promise((resolve, reject) => {
@@ -29,7 +18,7 @@ function binaryToBlob(options = {}) {
 
           const octetsArray = data.toJSON().data;
           resolve({
-            code: `export default octetsToBuffer(${JSON.stringify(octetsArray)});`,
+            code: `export default Uint8Array.from(${JSON.stringify(octetsArray)}).buffer;`,
             map: { mappings: '' },
           });
         });
