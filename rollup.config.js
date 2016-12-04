0c34dcbe3836fae2387b6bfd1c48022bd5d609d6
Chris LaRose <cjlarose@gmail.com>
2016-12-04 10:16:07 -0800

    Add binary to ArrayBuffer plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,42 @@
 import svelte from 'rollup-plugin-svelte';
 import buble from 'rollup-plugin-buble';
-import string from 'rollup-plugin-string';
+import { createFilter } from 'rollup-pluginutils';
+import { readFile } from 'fs';
+
+function binaryToBlob(options = {}) {
+  const filter = createFilter(options.include, options.exclude);
+  return {
+    name: 'binary-to-blob',
+    intro() {
+      return `function octetsToBuffer(octetsArray) {
+  var length = octetsArray.length;
+  var buffer = new ArrayBuffer(length);
+  var view = new DataView(buffer);
+  for (var i = 0; i < length; i++) {
+    view.setUint8(i, octetsArray[i]);
+  }
+  return buffer;
+}`;
+    },
+    load(id) {
+      if (!filter(id)) { return null; }
+      return new Promise((resolve, reject) => {
+        readFile(id, (err, data) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+
+          const octetsArray = data.toJSON().data;
+          resolve({
+            code: `export default octetsToBuffer(${JSON.stringify(octetsArray)});`,
+            map: { mappings: '' },
+          });
+        });
+      });
+    },
+  };
+}
 
 export default {
   entry: 'src/main.js',
@@ -10,7 +46,7 @@ export default {
     svelte({
       include: 'src/components/**.html',
     }),
-    string({
+    binaryToBlob({
       include: 'src/**/*.gz',
     }),
     buble(),
