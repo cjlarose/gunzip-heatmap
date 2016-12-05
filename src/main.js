3f245c11e02f1dd5672950501a65cb85dd804c26
Chris LaRose <cjlarose@gmail.com>
2016-12-04 20:58:53 -0800

    Add heat map component using fake data

--- src/main.js
+++ src/main.js
@@ -1,15 +1,38 @@
-import HelloWorld from './components/hello_world.html';
+import App from './components/app.html';
 import fish from './examples/fish.txt.gz';
 
 console.log(fish);
 
-const app = new HelloWorld({
+const app = new App({
   target: document.querySelector('main'),
   data: {
-    name: 'world',
-    files: {
-      'fish': fish,
-    },
+    octets: [
+      { data: 0x3c, numBits: 8 },
+      { data: 0x68, numBits: 8 },
+      { data: 0x31, numBits: 8 },
+      { data: 0x3e, numBits: 5 },
+      { data: 0x0a, numBits: 4 },
+
+      { data: 0x68, numBits: 2 },
+      { data: 0x65, numBits: 8 },
+      { data: 0x6c, numBits: 4 },
+      { data: 0x6c, numBits: 1 },
+      { data: 0x6f, numBits: 7 },
+      { data: 0x20, numBits: 10 },
+      { data: 0x77, numBits: 10 },
+      { data: 0x6f, numBits: 8 },
+      { data: 0x72, numBits: 2 },
+      { data: 0x6c, numBits: 6 },
+      { data: 0x64, numBits: 6 },
+      { data: 0x0a, numBits: 2 },
+
+      { data: 0x3c, numBits: 3 },
+      { data: 0x2f, numBits: 7 },
+      { data: 0x68, numBits: 0 },
+      { data: 0x31, numBits: 0 },
+      { data: 0x3e, numBits: 0 },
+      { data: 0x0a, numBits: 0 },
+    ],
   },
 });
 
